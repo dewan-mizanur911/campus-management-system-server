35f7c80031088d5cacd25682ed12572457624c1d
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-10-27 12:01:19 +0600

    middleware added

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const { MongoClient } = require("mongodb");
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jycgq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -17,7 +22,7 @@ async function run() {
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find({});
             const result = await cursor.toArray();
-            res.send(result);
+            res.json(result);
     })
         
     }
