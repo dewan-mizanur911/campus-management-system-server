fe3241e5bcdbb6e9b43f291071ff9ec3acc1fbf1
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-10-27 10:40:50 +0600

    initialized with index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const { MongoClient } = require("mongodb");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jycgq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        
+    const database = client.db("campusManagementSystem");
+    const serviceCollection = database.collection("services");
+        
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+    })
+        
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Campus Management Server Running at http://localhost:${port}`);
+});
