5160116db37fd6a2e610a51bb1e0037e20ebaafe
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-10-28 08:27:51 +0600

    updated

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient } = require("mongodb");
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const ObjectId = require('mongodb').ObjectId;
 
 //Middleware
 app.use(cors());
@@ -18,12 +19,35 @@ async function run() {
         
     const database = client.db("campusManagementSystem");
     const serviceCollection = database.collection("services");
+      const addedCollection = database.collection("addedServices");
         
-        app.get('/services', async (req, res) => {
-            const cursor = serviceCollection.find({});
-            const result = await cursor.toArray();
-            res.json(result);
-    })
+      // GET API Services
+      app.get('/services', async (req, res) => {
+        const cursor = serviceCollection.find({});
+        const result = await cursor.toArray();
+        res.json(result);
+      });
+      app.get('/addedServices', async (req, res) => {
+        const cursor = addedCollection.find({});
+        const result = await cursor.toArray();
+        res.json(result);
+      })
+
+    // POST API Add Services
+      app.post('/addedServices', async (req, res) => {
+        const order = req.body;
+          const result = await addedCollection.insertOne(order);
+
+        res.json(result);
+      });
+
+      // DELETE API service
+      app.delete('/addedServices/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await addedCollection.deleteOne(query);
+        res.json(result);
+      })
         
     }
     finally {
